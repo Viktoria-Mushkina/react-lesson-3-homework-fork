04c67f8f2054f990f5a8bd42883c933425560d92
Viktoria-Mushkina <vvmlulu@yandex.ru>
2025-11-07 17:07:39 +0300

    delete comment

--- src/modules/counter/contexts/counter-context.tsx
+++ src/modules/counter/contexts/counter-context.tsx
@@ -7,22 +7,3 @@ export type CounterContextType = {
  reset: () => void
 }
 export const CounterContext = createContext<CounterContextType | undefined>(undefined)
-// - counter-context.tsx: Определите типы и создайте контекст
-
-//     - Тип: CounterContextType с полями: count: number, increment: () => void, decrement: () => void, reset: () => void
-
-//     - Создайте CounterContext с помощью createContext
-
-// - counter-provider.tsx: Реализуйте провайдер
-
-//     - Используйте useState для хранения состояния счетчика
-
-//     - Начальное значение: 0
-
-//     - Реализуйте функции: increment, decrement, reset
-
-// - use-counter-context.ts: Создайте хук для использования контекста
-
-//     - Используйте useContext для получения контекста
-
-//     - Добавьте проверку на использование вне провайдера с понятным сообщением об ошибке
